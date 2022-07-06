3bcfb533713064ff05aac32e679ba465344e4733
Zhaagko <gilmutdinov.bogdazoe@gmail.com>
2022-07-06 17:56:19 +0300

    Database has been created

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+# Database
+from apps.database import db
 # Blueprints
 from apps.controller import module
 
@@ -7,5 +9,10 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(module)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
